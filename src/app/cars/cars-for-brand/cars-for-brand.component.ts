b87900cd81ccd5118024b8a888c4c670d3ff2a26
villarrealjavier <villarrealhinojojavier@gmail.com>
2023-06-12 12:06:28 +0200

    Fixed errors

--- src/app/cars/cars-for-brand/cars-for-brand.component.ts
+++ src/app/cars/cars-for-brand/cars-for-brand.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { cars } from '../../interfaces/cars.interface copy';
 import { ActivatedRoute } from '@angular/router';
 import {  CarsService } from '../cars.service';
+import { ShoppingService } from 'src/app/shopping-cart/services/shopping.service';
 
 @Component({
   selector: 'app-cars-for-brand',
@@ -14,7 +15,7 @@ export class CarsForBrandComponent {
   carsbyBrand:cars[]=[] //Lista de coches
 
   //Implementamos el activateRouter, para recoger el parametro y el servicio de coches
-  constructor(private route:ActivatedRoute,private service:CarsService){
+  constructor(private route:ActivatedRoute,private service:CarsService, private shoppingService:ShoppingService){
 
   }
   ngOnInit(){
@@ -25,5 +26,22 @@ export class CarsForBrandComponent {
         
       })
     })
+  }
+
+  //Añadir a la bolsa de la compra
+  addShoppingCart(car:cars){ // Recibirá un coche
+    let encontrado=false; // Variable que usaremos para asegurarnos de que el coche no esta ya en el carrito
+    for(let item of this.shoppingService.shoppingCart){ // Recorremos la lista de la compra
+      if(item.num_bastidor==car.num_bastidor){ //Comprobamos que no haya un coche con el mismo identificador
+        encontrado=true; //Si lo hay ponemos la variable a true
+      }
+    }
+    if(encontrado==false){ //Si por el contrario no lo hay, añadimos el coche al carrito y actualizamos la lista del sessionStorage
+      this.shoppingService.shoppingCart.push(car)
+      window.sessionStorage.setItem('carrito', JSON.stringify(this.shoppingService.shoppingCart));
+      
+
+
+    }
   }
 }
